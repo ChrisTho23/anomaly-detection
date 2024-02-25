0e64c6200749044308edfda67b33bc6fafa17373
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-02-25 14:17:28 +0100

    Add helper notebook

--- src/config.py
+++ src/config.py
@@ -2,13 +2,20 @@ from pathlib import Path
 
 DATA = {
     'corrupted': Path('../data/corrupted_emnist'),
-    'corrected': Path('../data/corrected_emnist'),
+    'corrected': Path('../data/corrected_emnist.npy'),
     'preds': Path('../data/preds'),
     'trues': Path('../data/trues'),
+    'masks': Path('../data/masks'),
+    'scores': Path('../data/scores'),
+    'cond': Path('../data/condition'),
 }
 
 MODEL = {
     'embedding_dim': 20,
     'image_size': 28, # quadratic pictures only
     'vae': Path('../models/vae.pth'),
+}
+
+EVALUATION = {
+    'cond': 0.15, # condition fo
 }
---
+++ src/find_annomalies.py
@@ -0,0 +1,29 @@
+import torch
+
+from config import DATA, EVALUATION
+
+if __name__ == "__main__":
+    # load torch tensors of predictions and true images (shape: (126300, 1, 28, 28))
+    preds = torch.load(DATA["preds"]) 
+    trues = torch.load(DATA["trues"])
+
+    # generate masks, i.e. list of tensros containing 
+    # absolute diff of each pixel to original image
+    masks = []
+
+    for pred, true in zip(preds, trues):
+        masks.append(abs(true-pred))
+
+    masks = torch.cat(masks, dim=0) # shape: (126300, 28, 28)
+    torch.save(masks, DATA["masks"])
+
+    # sum the values of anomalous pixels of all pixels in an image
+    # and weigh that by the number of pixels as evaluation metrics
+
+    scores = (masks.sum(1).sum(1) / (28 * 28)).detach().numpy()
+    torch.save(scores, DATA["scores"])
+
+    cond = scores > EVALUATION["condition"]
+    torch.save(cond, DATA["cond"])
+
+    print(f"Number of images with anomalies: {cond.sum()}")
