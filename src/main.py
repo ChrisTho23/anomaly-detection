a3ada0b9a89bca76e01ae6fc8bb31e9d14c1c736
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-02-25 14:49:14 +0100

    Add readme.md

--- src/main.py
+++ src/main.py
@@ -3,5 +3,5 @@ import subprocess
 if __name__ == "__main__":
     subprocess.run(['python', 'train.py'])
     subprocess.run(['python', 'prediction.py'])
-    subprocess.run(['python', 'find_annomalies.py'])
+    subprocess.run(['python', 'find_anomalies.py'])
     subprocess.run(['python', 'clean_up.py'])
